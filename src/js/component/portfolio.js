91945df20c854ac4461f16884ff1cbd30b5bbc38
dcbounds <boundsdarius@gmail.com>
2019-06-15 23:49:10 +0000

    Updated Portfolio with 4 cards

--- src/js/component/portfolio.js
+++ src/js/component/portfolio.js
@@ -4,82 +4,81 @@ export default class Portfolios extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1 className="card"> Beach Portfolio</h1>
-				<div>
-					<p>
-						{" "}
-						Below you will find some of the worlds finest places
-						where Team Mike resides on the weekends. Blah blah
-						beaches beaches beaches. We all love beaches.
-					</p>
-				</div>
+				<div className="cardcontain">
+					<h1> Beach Portfolio</h1>
+					<div>
+						<p>
+							{" "}
+							Below you will find some of the worlds finest places
+							where Team Mike resides on the weekends. Blah blah
+							beaches beaches beaches. We all love beaches.
+						</p>
+					</div>
+					<div className="card-deck">
+						<div className="card">
+							<img
+								className="card-img-top"
+								src="https://picsum.photos/300/180/?blur"
+								alt="Card image cap"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									This is a wider card with supporting text
+									below as a natural lead-in to additional
+									content. This content is a little bit
+									longer.
+								</p>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								className="card-img-top"
+								src="https://picsum.photos/300/180/?blur"
+								alt="Card image cap"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									This card has supporting text below as a
+									natural lead-in to additional content.
+								</p>
+							</div>
+						</div>
+						<div className="card">
+							<img
+								className="card-img-top"
+								src="https://picsum.photos/300/180/?blur"
+								alt="Card image cap"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									This is a wider card with supporting text
+									below as a natural lead-in to additional
+									content. This card has even longer content
+									than the first to show that equal height
+									action.
+								</p>
+							</div>
+						</div>
 
-				<div className="card-deck">
-					<div className="card mike">
-						<img
-							className="card-img-top"
-							src="https://ssl.tzoo-img.com/images/blog/legacyblog/us/wp-content/uploads/2018/02/1_BoracayIsland_WhiteBeach_Philippines_shutterstock_401484094.jpg?width=817&spr=1.5"
-							alt="Card image cap"
-						/>
-						<div className="card-body">
-							<h5 className="card-title">
-								<a href="">Boracay White Beach, Philippines</a>
-							</h5>
-							<p className="card-text">
-								This is one of the most beautiful Team Mike
-								owned beaches in the universe. Blah blah beaches
-								beaches beaches. We all love beaches.{" "}
-							</p>
-							<p className="card-text">
-								<small className="text-muted">
-									Last updated 3 mins ago
-								</small>
-							</p>
-						</div>
-					</div>
-					<div className="card mike">
-						<img
-							className="card-img-top"
-							src="https://ssl.tzoo-img.com/images/blog/legacyblog/us/wp-content/uploads/2018/02/3_NaPaliCoast_Kauai_Hawaii_shutterstock_618002255.jpg?width=817&spr=1.5"
-							alt="Card image cap"
-						/>
-						<div className="card-body">
-							<h5 className="card-title">
-								<a href="">Na Pali Coast, Kauai, Hawaii</a>
-							</h5>
-							<p className="card-text">
-								Blah Blah...This is one of the most beautiful
-								Team Mike owned beaches in the universe. Blah
-								blah beaches beaches beaches. We all love
-								beaches.
-							</p>
-							<p className="card-text">
-								<small className="text-muted">
-									Last updated 3 mins ago
-								</small>
-							</p>
-						</div>
-					</div>
-					<div className="card mike">
-						<img
-							className="card-img-top"
-							src="https://ssl.tzoo-img.com/images/blog/legacyblog/us/wp-content/uploads/2018/02/9_BinginBeach_Bali_shutterstock_628214399.jpg?width=820"
-							alt="Card image cap"
-						/>
-						<div className="card-body">
-							<h5 className="card-title">
-								<a href="">Bingin Beach, Bali, Indonesia</a>
-							</h5>
-							<p className="card-text">
-								This is one of the most beautiful Team Mike
-								owned beaches in the universe. Blah blah beaches
-								beaches beaches. We all love beaches.
-							</p>
-							<p className="card-text">
-								<small className="text-muted">
-									Last updated 3 mins ago
-								</small>
-							</p>
+						<div className="card">
+							<img
+								className="card-img-top"
+								src="https://picsum.photos/300/180/?blur"
+								alt="Card image cap"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									This is a wider card with supporting text
+									below as a natural lead-in to additional
+									content. This card has even longer content
+									than the first to show that equal height
+									action.
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
